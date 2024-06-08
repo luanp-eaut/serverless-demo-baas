8fe970ef4e6ae011e82ebe248849f8ab4131ddd2
luanp-eaut <luanp@eaut.edu.vn>
2024-06-08 16:05:12 +0700

    update

--- app/components/FirebaseConfig/FirebaseConfig.ts
+++ app/components/FirebaseConfig/FirebaseConfig.ts
@@ -2,7 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 function FirebaseConfig() {
-  const firebaseConfig = {};
+  const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+  };
 
   const app = initializeApp(firebaseConfig);
   return getDatabase(app);
