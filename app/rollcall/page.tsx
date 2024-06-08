14ab75ae75d480cead28d29e4e04279bf3cc6edf
luanp-eaut <luanp@eaut.edu.vn>
2024-06-08 17:06:22 +0700

    update

---
+++ app/rollcall/page.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+const RollcallPage = () => {
+  return <Container sx={{ height: "100vh", width: "100vw", bgcolor: "red" }} />;
+};
+
+export default RollcallPage;
