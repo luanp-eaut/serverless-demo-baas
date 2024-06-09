21dab4e32f3c9fb0f8d4758c24281131a7933aef
luanp-eaut <luanp@eaut.edu.vn>
2024-06-09 09:29:11 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { FirebaseCrud } from "./components/FirebaseCrud/FirebaseCrud";
-
+import AboutSection from "@/components/AboutSection"
+import HeroSection from "@/components/HeroSection"
+import ProjectsSection from "@/components/ProjectsSection"
 export default function Home() {
-  return <FirebaseCrud />;
+  return (
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+    </main>
+  )
 }
